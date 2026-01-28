1a726159d2f39a27fe78f775814f1142f544a4f2
Juan Diego Bonilla <75050568+Juandiegodesign@users.noreply.github.com>
2026-01-28 06:45:52 +0000

    ADDED comment

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -1,12 +1,24 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 02_10
+// Exercise 02_10 MODDED 
 // Type Casting, by Eduardo Corpeño 
 
 #include <iostream>
 #include <cstdint>
 
 int main(){
-    
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -123.45f;
+    sprite_x = target_x;
+    player_x = sprite_x;
+
+    std::cout << "Target X (float): " << target_x << std::endl;
+    std::cout << "Sprite X (int32_t): " << sprite_x << std::endl;
+    std::cout << "player X (uint32_32t): " << static_cast<int32_t> (player_x) << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
